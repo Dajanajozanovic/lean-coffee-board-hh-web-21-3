b03e3a21e9b28b59d2d942f97bf97b56b7d042fe
Dajana Jozanovic <dajana.jozanovic@t-online.de>
2021-06-02 16:38:04 +0200

    add card with author

--- client/src/components/Card.spec.js
+++ client/src/components/Card.spec.js
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react'
 import Card from './Card'
 
 describe('Card', () => {
-  it('renders its children', () => {
-    render(<Card>Hello world</Card>)
-    expect(screen.getByText('Hello world')).toBeInTheDocument()
+  it('renders text and author', () => {
+    render(<Card text="this is a text" author="Jane Doe" />)
+    expect(screen.getByText('this is a text', 'Jane Doe')).toBeInTheDocument()
   })
 })
